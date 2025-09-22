5878596420c7c5917ea8f77f0810ffca83596110
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-09-22 13:16:20 +0530

    message

--- 05-password.sh
+++ 05-password.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-echo "Enter your password: "
-read pin_number
+# echo "Enter your password: "
+#read pin_number
+
+#echo " you have entered: $pin_number " 
+
+
+echo
+
+echo "Enter your password"
+
+read -s pin_number
 
 echo " you have entered: $pin_number "
