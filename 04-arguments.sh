2a6485bd901b64e8a37db3dd38686ce1ff09e8e6
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-09-22 12:53:22 +0530

    message

---
+++ 04-arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1 #takes input from command line argument
+PERSON2=$2 #takes input from command line argument
+
+echo "$PERSON1:: Hi $PERSON2, how are you?"
+echo "$PERSON2:: Hi $PERSON1, I am good. how are you?"
+echo "$PERSON1:: I am good $PERSON2, what are you learning recently?"
+echo "$PERSON2:: I am learning shell scripting $PERSON1"
