3aa9a068abfeaf23de4ed6fa96464b4fdc4093c1
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-09-22 12:50:15 +0530

    message

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:: Hi $PERSON2, how are you?"
+echo "$PERSON2:: Hi $PERSON1, I am good. how are you?"
+echo "$PERSON1:: I am good $PERSON2, what are you learning recently?"
+echo "$PERSON2:: I am learning shell scripting $PERSON1"
