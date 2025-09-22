4ee472d174634a8751fa985ae8ec395d076cde90
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-09-22 12:44:16 +0530

    message

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi suresh, how are you?"
+echo "suresh:: Hi Ramesh, I am good. how are you?"
+echo "Ramesh:: I am good suresh, what are you learning recently?"
+echo "suresh:: I am learning shell scripting Ramesh"
