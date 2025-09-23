e4b792c2d0af9cd69b41a497584732586f1b3cbb
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-09-23 17:33:44 +0530

    moving day 15

---
+++ 10-conditions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+number=$1
+
+if [ $number -lt 10 ]; then
+    echo "given number $number is less than 10"
+elif[ $number -eq 10 ]; then
+    echo "given number $number is equal to 10"
+else
+    echo "given number $number is greater than 10 "
+
+fi
+
+
+# greater than -gt
+# less than -lt
+# equal -eq
---
+++ 11-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter a number to check it is even or odd : "
+
+read number
+
+if [ $($number % 2) -eq 0 ]; then
+    echo "Entered number $number is even "
+else
+    echo "Entered number $number is odd"
+fi
