4282b4816ef143076ba96c5b5b560033c2fd93bb
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-09-24 10:33:22 +0530

    syncing repository

--- 13-installation.sh
+++ 13-installation.sh
@@ -5,12 +5,12 @@
 
 user=$(id -u)
 
-if [$user -gt 0]; then
+if [ $user -gt 0 ]; then
     echo "ERROR:: you are not allowed to run this script use root privilege"
     #exit 1
 fi
 
-if [$user -gt 0]; then
+if [ $user -gt 0 ]; then
     #you have stop the execution of script and exit from the flow you can add exit code in above condition to avoid extra lines of code
     exit 1
 fi
