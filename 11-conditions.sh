7f731277d6dc30868a598eb8be3b8fc218edd605
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-09-24 10:25:51 +0530

    syncing repository

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -4,7 +4,7 @@ echo "Enter a number to check it is even or odd : "
 
 read number
 
-if [ $($number % 2) -eq 0 ]; then
+if [ $(($number % 2)) -eq 0 ]; then
     echo "Entered number $number is even "
 else
     echo "Entered number $number is odd"
