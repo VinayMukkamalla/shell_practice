561f8198b26e1ced1e1aab0dc62f56f7d8efe674
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-09-29 15:46:39 +0530

    added changes

--- 21-set.sh
+++ 21-set.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+set -e
+
+echo "we will know about set -e option to replace VALIDATE function"
+
+echo "we are setting error to raise using set -e, or set -euo pipefail "
+
+echo "before error0"
+
+sdbbhjg;dnf
+
+echo "after error"
