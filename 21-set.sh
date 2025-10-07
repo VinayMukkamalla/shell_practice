d537a48b04b86abfe4904a0ccb93c8cf8845eca3
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-10-07 11:41:43 +0530

    adding to repo

--- 21-set.sh
+++ 21-set.sh
@@ -15,7 +15,7 @@ echo "we will know about set -e option to replace VALIDATE function"
 
 echo "we are setting error to raise using set -e, or set -euo pipefail "
 
-echo "before error0"
+echo "before error"
 
 sdbbhjg;dnf #here shell understands there is an error and ERR is the signal
 
