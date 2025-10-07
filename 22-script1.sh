90925c2b29c05ecdaaf0f14aa0ab3c707355764d
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-10-07 18:22:05 +0530

    adding to repo

---
+++ 22-script1.sh
@@ -0,0 +1,5 @@
+NAME=INDIA
+echo "MY Country is : $NAME"
+
+echo "print script1 PID: $$"
+sh 23-script2.sh
---
+++ 23-script2.sh
@@ -0,0 +1,5 @@
+sh 22-script1.sh
+
+echo "MY Country is : $NAME"
+
+echo "print script2 PID: $$"
