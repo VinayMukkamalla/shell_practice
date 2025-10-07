a7b733f39c407ad4c14c33466403f865c4e80561
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-10-07 11:24:40 +0530

    adding to repo

--- 21-set.sh
+++ 21-set.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-trap 'echo "There is an error in $LINENO, and the command errored is $BASH_COMMAND"' ERR 
+error(){
+    #error function
+    echo "there is an error $LINENO, command is : $BASH_COMMAND"
+}
+
+# trap will execuete when there is an error trap is a command shell will call trap by default ehen there is an ERR
+
+trap error ERR 
 
 echo "we will know about set -e option to replace VALIDATE function"
 
@@ -10,6 +17,6 @@ echo "we are setting error to raise using set -e, or set -euo pipefail "
 
 echo "before error0"
 
-sdbbhjg;dnf
+sdbbhjg;dnf #here shell understands there is an error and ERR is the signal
 
 echo "after error"
