3092e88c07e5b2e602e572213eb4b8c5ca9dc952
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-10-11 11:59:38 +0530

    pushing files 24 & 25 logs backup and delete

---
+++ 24-delete-old-logs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+user=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+START_TIME=$(date +%s)
+mkdir -p $LOG_FOLDER
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+echo " script started execution at : $(date)" | tee -a $LOG_FILE
+
+if [ $user -gt 0 ]; then
+    echo "ERROR:: you are not allowed to run this script use root privilege"
+    exit 1
+fi
+
+SOURCE_DIR=/home/ec2-user/app-logs/
+
+if [ ! -d $SOURCE_DIR ]; then
+    echo "source directory $SOURCE_DIR does not exist "
+    exit 1
+fi
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
+
+
+
+while IFS= read -r filepath >>&$LOG_FILE
+do
+    echo "file $filepath will be deleted"
+    rm -rf $filepath
+    echo "$filepath deletion completed"
+
+done <<<$FILES_TO_DELETE
+
+# I will check source dir exist or not
+
+# then find out the files 
+
+# log the files that re going to be deleted 
+
+# then delete the log files
+
---
+++ 25-backup-zip.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+user=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+START_TIME=$(date +%s)
+mkdir -p $LOG_FOLDER
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+echo " script started execution at : $(date)" | tee -a $LOG_FILE
+
+if [ $user -gt 0 ]; then
+    echo "ERROR:: you are not allowed to run this script use root privilege"
+    exit 1
+fi
+
+SOURCE_DIR=$1
+
+DEST_DIR=$2
+
+DAYS=${3:-14}
+
+if [ ! -d $SOURCE_DIR ]; then
+    echo "$SOURCE_DIR does not exist"
+    exit 1
+fi
+
+if [ ! -d $DEST_DIR ]; then
+    echo "$DEST_DIR does not exist"
+    exit 1
+fi
+
+usage(){
+    echo "ERROR:: file $0 usage manadatory parameters <source dir> <destination dir> <days>(by default 14 days if not provided )"
+    exit 1
+}
+
+if [ $# -le 2 ]; then
+    usage
+fi
+
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
+
+
+
+# source dir
+
+# destination directory
+
+# find the files older than  14 days
+
+# zip them
+
+# move to destination 
+
+# some team will pick the files and keep them in storage servers
+
+# then delete the files in source directory
+
+#Admin task if you have to have access to all folders we should have admin access
+
+#script usaage we should pass variables source dir and dest dir and days {no of days or older log files by days to delete}
+
+#out of 3 args 2(source dir,dest dir) are mandatory and 1(days) is optional
+
+# usage is a function that is option it defines the usage of this script 
+#if no parameters are passed then suggests user to run by passing 2 args which are manadtory
+# $# count of args passed through cli if $# -lt 2 raise error
+# first define usage(){} then call in condition [ $# -lt 2 ]
+
+#check if both dource dir and destin dir exits individually and raise error if they don't exist
+
+#add uniqueness to the log file using $(data +%F-%H-%M)
+
