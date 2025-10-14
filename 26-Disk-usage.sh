460ac519e93f3011be3b2b228bc36d17f15c6b22
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-10-14 15:52:07 +0530

    Disk usage monitoring and reporting

--- 26-Disk-usage.sh
+++ 26-Disk-usage.sh
@@ -4,14 +4,19 @@ DISK_USAGE=$(df -hT | grep -v Filesystem)
 
 DISK_THRESHOLD=2
 
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+MESSAGE=""
+
 while IFS= read -r line
 do 
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        echo "High Disk utilisation on $PARTITION : $USAGE"
+        MESSAGE="High Disk utilisation on $PARTITION : $USAGE%"
     else
         echo "Disk usage normal"
     fi
 
 done <<< $DISK_USAGE
+
+echo "Message body: $MESSAGE"
